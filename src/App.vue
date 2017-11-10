b00632c8185adc5c28c6910fdf521484603596fb
Gabriel <gabriel.tagliari93@gmail.com>
2017-11-10 20:07:01 -0200

    Criaçãp de CSS e do componente ImagemResponsiva

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 
   <h1 class="centralizado">{{titulo}}</h1>
 
-  <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por parte do título">
+  <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="Filtre por parte do título">
 
   <ul class="lista-fotos">
-  <li class="lista-fotos-item" v-for="foto of fotos">
+  <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
     <meu-painel :titulo="foto.titulo">
-      <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+      <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
     </meu-painel>
   </li>
   </ul>
@@ -18,11 +18,13 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
 
   components: {
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   data() {
@@ -30,6 +32,17 @@ export default {
       titulo: 'Alurapic',
       fotos: [],
       filtro: ''
+    }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
     }
   },
 
@@ -58,10 +71,6 @@ export default {
 
   .lista-fotos .lista-fotos-item {
     display: inline;
-  }
-
-  .imagem-responsiva {
-      width: 100%;
   }
 
   .filtro {
