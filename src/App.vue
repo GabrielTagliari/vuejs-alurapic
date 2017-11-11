98a527a7b228a1a248d08ee05e20a03f58f99bbd
Gabriel <gabriel.tagliari93@gmail.com>
2017-11-10 22:43:46 -0200

    Refatorações, criação botao padrão e configuração das rotas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="corpo">
 
+    <meu-menu :rotas="routes"></meu-menu>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
+
+<script>
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
+export default {
+
+  components: {
+    'meu-menu': Menu
+  },
+
+  data() {
+    return {
+      routes
+    }
+  }
+}
 
 </script>
 
@@ -11,5 +33,13 @@
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity 0.4s;
   }
 </style>
